83d0d88bef132cae465db647036ac8367e7c9c80
aryputh <aryanputhran@gmail.com>
2025-07-18 12:20:05 -0700

    Tried a fix for the error

--- utils/event_form.py
+++ utils/event_form.py
@@ -31,7 +31,7 @@ def render_event_form(time_format, start_hour, end_hour, days_options):
         with col2:
             start_time = tu.get_time_from_inputs(col2, "Start Time", start_hour.hour, time_format, "event_start", True)
             end_time = tu.get_time_from_inputs(col2, "End Time", end_hour.hour, time_format, "event_end", True)
-            color = st.color_picker("Color *", value = st.session_state["event_color"], key = "event_color")
+            color = st.color_picker("Color *", value = st.session_state["event_color"], key = "temp_event_color")
 
         submitted = st.form_submit_button("Add Event")
 
@@ -59,7 +59,8 @@ def render_event_form(time_format, start_hour, end_hour, days_options):
                     }
                     st.session_state["events"].append(event)
 
+                st.success(f"Event '{title}' added to: {', '.join(selected_days)}")
+
                 # Select new random color
                 st.session_state["event_color"] = random.choice(COLOR_PALETTE)
-
-                st.success(f"Event '{title}' added to: {', '.join(selected_days)}")
+                st.session_state["temp_event_color"] = st.session_state["event_color"]
