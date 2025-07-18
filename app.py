ee16b6b299da3f73802b04d5ea0610544864f7d1
aryputh <aryanputhran@gmail.com>
2025-07-17 19:24:36 -0700

    Added more sidebar options

--- app.py
+++ app.py
@@ -13,10 +13,15 @@ if "events" not in st.session_state:
 st.sidebar.header("Calendar Settings")
 
 # Days of the week
+days_options = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+visible_days = st.sidebar.multiselect("Select Days to Show", options = days_options, default = days_options[1:6])
 
 # Time range
+start_hour = st.sidebar.slider("Start Hour", 0, 23, 8)
+end_hour = st.sidebar.slider("End Hour", 1, 24, 18)
 
 # Time format
+time_format = st.sidebar.radio("Time Format", ["12-hour", "24-hour"])
 
 # Main Area: Event Input
 
