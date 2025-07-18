02cc6d31b606851ee0fdc56669be252521d2bdca
aryputh <aryanputhran@gmail.com>
2025-07-18 12:00:35 -0700

    Changed default values

--- utils/render_calendar.py
+++ utils/render_calendar.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 
 def render_calendar(events, visible_days, start_hour, end_hour, time_format):
-    # Vertical spacing
-    padding_px = 2
+    # Vertical spacing for events
+    vertical_event_padding_px = 4
+    horizontal_event_padding_px = 2
 
-    hour_height_px = 60
-    day_height_px = hour_height_px * (end_hour.hour - start_hour.hour)
+    # Height of hour rows
+    vertical_hour_height_px = 60
+    vertical_day_height_px = vertical_hour_height_px * (end_hour.hour - start_hour.hour)
     column_width = f"{100 / len(visible_days)}%"
 
     # Generate time labels (left sidebar)
@@ -18,7 +20,7 @@ def render_calendar(events, visible_days, start_hour, end_hour, time_format):
         else:
             label = raw_label.strftime("%H:00")
 
-        time_labels_html += f'<div class = "time-label" style = "height: {hour_height_px}px">{label}</div>'
+        time_labels_html += f'<div class = "time-label" style = "height: {vertical_hour_height_px}px">{label}</div>'
     
     # Generate grid columns for days
     day_columns_html = ""
@@ -33,12 +35,11 @@ def render_calendar(events, visible_days, start_hour, end_hour, time_format):
             start_offset = (start_time.hour + start_time.minute / 60) - start_hour.hour
             duration = (end_time - start_time).seconds / 3600
 
-            top_px = start_offset * hour_height_px + padding_px
-            height_px = duration * hour_height_px - (padding_px * 2)
+            height_px = duration * vertical_hour_height_px - (vertical_event_padding_px * 2)
 
             events_html += (
                 '<div class = "event" style = "'
-                f'top: {top_px}px;'
+                f'top: {horizontal_event_padding_px}px;'
                 f'height: {height_px}px;'
                 f'background-color: {event["color"]};">'
                 f'<div class = "event-title">{event["title"]}</div>'
@@ -48,7 +49,7 @@ def render_calendar(events, visible_days, start_hour, end_hour, time_format):
         day_columns_html += (
             f'<div class = "day-column" style = "width" {column_width};">'
             f'<div class = "day-header">{day}</div>'
-            f'<div class = "day-body" style = "height: {day_height_px}px;">{events_html}</div>'
+            f'<div class = "day-body" style = "height: {vertical_day_height_px}px;">{events_html}</div>'
             '</div>'
         )
 
